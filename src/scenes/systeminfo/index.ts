af559aa87d94d252705b041d9c508f7c075cb65a
Anton Potekhin <anton.potekhin@orbitsoft.com>
2023-06-02 15:24:26 +0300

    sensors fix

--- src/scenes/systeminfo/index.ts
+++ src/scenes/systeminfo/index.ts
@@ -16,13 +16,21 @@ interface MemInfo {
     percent: number
 }
 
+interface SensorsInfo {
+    label: string
+    value: number
+    unit: string
+    type: string
+    key: string
+}
+
 const systemInfoScene = new Scenes.BaseScene<Scenes.SceneContext>("systeminfo")
 systemInfoScene.enter(async ctx => {
     ctx.reply("Start fetching....")
     const p = exec("glances -t=1 --quiet -w --disable-webui >/dev/null")
     let cpuInfo: CPUInfo
     let memInfo: MemInfo
-    let sensorsInfo: MemInfo
+    let sensorsInfo: Array<SensorsInfo> = []
     while (!cpuInfo) {
         await delay(1000)
         try {
@@ -31,10 +39,8 @@ systemInfoScene.enter(async ctx => {
             response = await axios.get("http://127.0.0.1:61208/api/3/mem")
             memInfo = response.data as MemInfo
             response = await axios.get("http://127.0.0.1:61208/api/3/sensors")
-            sensorsInfo = response.data
-        } catch (e) {
-
-        }
+            sensorsInfo = response.data as Array<SensorsInfo>
+        } catch (e) { }
     }
     // Dirty trick +1 because exec return shell script that start glances 
     try {
@@ -46,13 +52,13 @@ systemInfoScene.enter(async ctx => {
     if (Object.entries(sensorsInfo).length > 0) {
         sensorsStr = "<b>Sensors:</b>\n"
         Object.entries(sensorsInfo).forEach(([key, value], index) => {
-            sensorsStr.concat(`${key}:<i>value</i>\n`)
+            sensorsStr.concat(`${value.label}:<i>${value.value} ${value.unit}</i>\n`)
         })
     }
 
     const memInfoStr = `<b>MemInfo:</b> <i>${memInfo.percent}%</i> (free: <i>${humanFileSize(memInfo.free)}</i> from: <i>${humanFileSize(memInfo.total)}</i>)\n`
     const cpuInfoStr = `<b>CPUInfo:</b> <i>${cpuInfo.total}%</i>\n`
-    ctx.replyWithHTML(cpuInfoStr+memInfoStr+sensorsStr)
+    ctx.replyWithHTML(cpuInfoStr + memInfoStr + sensorsStr)
 })
 
 export default systemInfoScene
