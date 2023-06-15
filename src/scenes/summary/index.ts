9b5f33672927dc1c251282d045bf0d6fe01a845f
Anton Potekhin <anton.potekhin@orbitsoft.com>
2023-06-15 18:58:29 +0300

    improve

--- src/scenes/summary/index.ts
+++ src/scenes/summary/index.ts
@@ -5,7 +5,6 @@ import { getHTMLReport as torrserverHtmlReport } from '../../util/torrserver'
 
 const summaryScene = new Scenes.BaseScene<Scenes.SceneContext>('summary')
 summaryScene.enter(async (ctx) => {
-  ctx.replyWithHTML("Yo, Ho! Fetching...")
   let fullReport: string | null = null
   const reports = [qbittorentHtmlReport(true), plexHtmlReport(true), torrserverHtmlReport(true)]
   Promise.all(reports).then(_reports => {
