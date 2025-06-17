495a25d666f1ffa1b9a6cf03f5ceaa36429330c4
Anton Potekhin <anton.potekhin@orbitsoft.com>
2025-06-18 00:00:15 +0300

    .

--- src/scenes/caffeine/index.ts
+++ src/scenes/caffeine/index.ts
@@ -24,27 +24,24 @@ const disableCaffeine = (ctx: Scenes.SceneContext) => {
 
 const caffeineScene = new Scenes.BaseScene<Scenes.SceneContext>('caffeine')
 caffeineScene.enter(async (ctx) => {
-  fs.stat(process.env.CAFFEINE_FILE_PATH, (exists) => {
-    if (exists == null) {
-      ctx.reply(
-        'Caffeine mode disabled! Do you want to turn Caffeine mode on?',
-        Markup.inlineKeyboard([
-          Markup.button.callback('Yes', 'Enable_Yes'),
-          Markup.button.callback('No', 'Enable_No'),
-        ])
-      )
-    } else if (exists.code === 'ENOENT') {
-      ctx.reply(
-        'Caffeine mode enabled! Do you want to turn Caffeine mode off?',
-        Markup.inlineKeyboard([
-          Markup.button.callback('Yes', 'Disable_Yes'),
-          Markup.button.callback('No', 'Disable_No'),
-        ])
-      )
-    }
-  });
 
-
+  if (fs.existsSync(process.env.CAFFEINE_FILE_PATH)) {
+    ctx.reply(
+      'Caffeine mode enabled! Do you want to turn Caffeine mode off?',
+      Markup.inlineKeyboard([
+        Markup.button.callback('Yes', 'Disable_Yes'),
+        Markup.button.callback('No', 'Disable_No'),
+      ])
+    )
+  } else {
+    ctx.reply(
+      'Caffeine mode disabled! Do you want to turn Caffeine mode on?',
+      Markup.inlineKeyboard([
+        Markup.button.callback('Yes', 'Enable_Yes'),
+        Markup.button.callback('No', 'Enable_No'),
+      ])
+    )
+  }
   return
 })
 
