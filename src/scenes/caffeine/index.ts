6f529cb4381769430d6b0c7141be75bffa1d6a5f
Anton Potekhin <anton.potekhin@orbitsoft.com>
2025-06-26 09:55:03 +0300

    Improved coffeine

--- src/scenes/caffeine/index.ts
+++ src/scenes/caffeine/index.ts
@@ -1,25 +1,8 @@
 import { Markup, Scenes } from 'telegraf'
 import { isCaffeineModeEnabled, enableCaffeine, disableCaffeine } from '../../util/caffeine'
 
-const doEnableCaffeine = (ctx: Scenes.SceneContext) => {
-  try {
-    enableCaffeine()
-    ctx.reply('Caffeine dose taken!')
-  } catch (err) {
-    console.error('Can\'t enable Caffeine mode:', err);
-  }
-  ctx.scene.leave()
-}
 
-const doDisableCaffeine = (ctx: Scenes.SceneContext) => {
-  try {
-    disableCaffeine()
-    ctx.reply('Caffeine has been removed.')
-  } catch (err) {
-    console.error('Can\'t disable Caffeine mode:', err);
-  }
-  ctx.scene.leave()
-}
+
 
 const caffeineScene = new Scenes.BaseScene<Scenes.SceneContext>('caffeine')
 caffeineScene.enter(async (ctx) => {
@@ -28,24 +11,44 @@ caffeineScene.enter(async (ctx) => {
     ctx.reply(
       'Caffeine mode enabled! Do you want to turn Caffeine mode off?',
       Markup.inlineKeyboard([
-        Markup.button.callback('Yes', 'Disable_Yes'),
-        Markup.button.callback('No', 'Disable_No'),
+        Markup.button.callback('Yes', 'Disable_Yes', true),
+        Markup.button.callback('No', 'Disable_No', true),
       ])
     )
   } else {
     ctx.reply(
-      'Caffeine mode disabled! Do you want to turn Caffeine mode on?',
+      'What dose of caffeine should I take??',
       Markup.inlineKeyboard([
-        Markup.button.callback('Yes', 'Enable_Yes'),
-        Markup.button.callback('No', 'Enable_No'),
+        Markup.button.callback('30 min', 'Enable_Yes:30', true),
+        Markup.button.callback('1 hour', 'Enable_Yes:60', true),
+        Markup.button.callback('2 hours', 'Enable_Yes:120', true),
+        Markup.button.callback('3 hours', 'Enable_Yes:180', true),
+        Markup.button.callback('Cancel', 'Enable_No', true),
       ])
     )
   }
   return
 })
 
-caffeineScene.action('Enable_Yes', doEnableCaffeine)
-caffeineScene.action('Disable_Yes', doDisableCaffeine)
+caffeineScene.action(/Enable_Yes:(.+)/, (ctx) => {
+  try {
+    const threshold = parseInt(ctx.match[1]) * 60;
+    enableCaffeine(threshold)
+    ctx.reply('Caffeine dose taken!')
+  } catch (err) {
+    console.error('Can\'t enable Caffeine mode:', err);
+  }
+  ctx.scene.leave()
+})
+caffeineScene.action('Disable_Yes', (ctx) => {
+  try {
+    disableCaffeine()
+    ctx.reply('Caffeine has been removed.')
+  } catch (err) {
+    console.error('Can\'t disable Caffeine mode:', err);
+  }
+  ctx.scene.leave()
+})
 
 caffeineScene.action('Enable_No', (ctx) => {
   ctx.reply('I agree that Сaffeine is harmful to health! Right choice!')
--- src/util/caffeine.ts
+++ src/util/caffeine.ts
@@ -4,8 +4,8 @@ export function isCaffeineModeEnabled(): boolean {
   return fs.existsSync(process.env.CAFFEINE_FILE_PATH)
 }
 
-export function enableCaffeine() {
-  fs.writeFileSync(process.env.CAFFEINE_FILE_PATH, 'caffeine', 'utf-8');
+export function enableCaffeine(threshold: number) {
+  fs.writeFileSync(process.env.CAFFEINE_FILE_PATH, threshold.toString(), 'utf-8');
 }
 
 export function disableCaffeine() {
